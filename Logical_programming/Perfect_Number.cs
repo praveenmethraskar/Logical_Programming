ad533b1916a86afcf1482f2ed98c9ec5f9c08b63
praveenmethraskar <praveenmethraskar@gmail.com>
2022-08-08 21:04:58 +0530

    Perfect Number

---
+++ Logical_programming/Perfect_Number.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_programming
+{
+    
+
+    public class Perfect_Number
+    {
+        int num, sum = 0, i;
+
+        public void PerfectNumber()
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("Enter A Number");
+
+            num = Int32.Parse(Console.ReadLine());
+
+            for (int i = 1; i < num; i++)
+            {
+                if  (num % i == 0)
+                {
+                    Console.WriteLine(i+" ");
+                    sum = sum + i;
+                }
+            }
+
+            if (sum == num)
+            {
+                Console.WriteLine("Perfect Number");
+            }
+            else
+            {
+                Console.WriteLine("Not a perfect number");
+            }
+        }
+
+    }
+}
--- Logical_programming/Program.cs
+++ Logical_programming/Program.cs
@@ -3,17 +3,31 @@
 namespace Logical_programming
 {
     public class Program
-    {
-
+    { 
         public static void Main(string[] args)
-        {
-
-
-            Fibonacci_Series_child obj = new Fibonacci_Series_child();
-
-            obj.Series();
-            obj.Display();
-
+        { 
+            bool isrun = true;
+            while(isrun)
+            {
+                Console.WriteLine("Select:\n1)Fibonacii Series \n2)Perfect Number");
+                int option = Int32.Parse(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        Fibonacci_Series_child obj = new Fibonacci_Series_child();
+                        obj.Series();
+                        obj.Display();
+                        break;
+                    case 2:
+                        Perfect_Number perfect_Number = new Perfect_Number();
+                        perfect_Number.PerfectNumber();
+                        break;
+                    default:
+                        isrun = false;
+                        Console.WriteLine("select correct option");
+                        break;
+                }
+            }  
         }
     }
 }
